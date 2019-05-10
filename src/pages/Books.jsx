43b951970ea7ac8fc2f57c7c5f4fcda5329c44ee
AntonVoronezh <pstut@mail.ru>
2019-05-10 19:05:05 +0300

    add filter in Books

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 
-import books from '../data/books';
+import data from '../data/books';
 import Card from '../components/Card';
 
-function Books() {
-    return (
-        <main id="books">
-            <div className="mdc-layout-grid">
-                <div className="mdc-layout-grid__inner">
-                    {books.map((book, index) =>
-                        <div key={index} className="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
-                            <Card book={book} />
-                        </div>
-                    )}
-                </div>
-            </div>
-        </main>
-    );
+function Books({ match }) {
+	const books = data.filter(b => b.topic === match.params.topic);
+	return (
+		<main id="books">
+			<div className="mdc-layout-grid">
+				<div className="mdc-layout-grid__inner">
+					{books.map((book, index) => (
+						<div key={index} className="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+							<Card book={book} />
+						</div>
+					))}
+				</div>
+			</div>
+		</main>
+	);
 }
 
 export default Books;
