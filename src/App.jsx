29343eb196495cdf7f131aa547197c0e01abe79b
AntonVoronezh <pstut@mail.ru>
2019-05-10 21:08:53 +0300

    change BrowserRouter from App to index

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 import Toolbar from './components/Toolbar';
 import Content from './components/Content';
@@ -28,39 +28,32 @@ class App extends Component {
 
 	render() {
 		return (
-			<BrowserRouter>
-				<div className="app">
-					<Toolbar user={this.state.user} />
+			<div className="app">
+				<Toolbar user={this.state.user} />
 
-					<Content>
-						<Route path="/books" component={Sidenav} />
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route path="/about" component={About} />
-							<Route path="/login" render={props => <Login onLogin={this.login} />} />
-							<Route path="/logout" render={props => <Logout onLogout={this.logout} />} />
-							{/* <Route exact path="/books" component={Books} /> */}
-							{/* <Route exact path="/books/:topic?" component={Books} /> */}
-							{/* <Route path="/books/:topic/:Book" component={Book} /> */}
-							<PrivateRoute
-								exact
-								path="/books/:topic?"
-								user={this.state.user}
-								component={Books}
-								data={data}
-							/>
-							<PrivateRoute
-								path="/books/:topic/:Book"
-								user={this.state.user}
-								component={Book}
-								data={data}
-							/>
+				<Content>
+					<Route path="/books" component={Sidenav} />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/about" component={About} />
+						<Route path="/login" render={props => <Login onLogin={this.login} />} />
+						<Route path="/logout" render={props => <Logout onLogout={this.logout} />} />
+						{/* <Route exact path="/books" component={Books} /> */}
+						{/* <Route exact path="/books/:topic?" component={Books} /> */}
+						{/* <Route path="/books/:topic/:Book" component={Book} /> */}
+						<PrivateRoute
+							exact
+							path="/books/:topic?"
+							user={this.state.user}
+							component={Books}
+							data={data}
+						/>
+						<PrivateRoute path="/books/:topic/:Book" user={this.state.user} component={Book} data={data} />
 
-							<Route component={NotFound} />
-						</Switch>
-					</Content>
-				</div>
-			</BrowserRouter>
+						<Route component={NotFound} />
+					</Switch>
+				</Content>
+			</div>
 		);
 	}
 }
