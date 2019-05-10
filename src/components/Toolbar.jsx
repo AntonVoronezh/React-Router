1e12245f2c0ad83dd3f4611a456347b6b4412dc1
AntonVoronezh <pstut@mail.ru>
2019-05-10 18:01:43 +0300

    add activeClassName

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 function Toolbar({ user }) {
-    return (
-        <header className="mdc-toolbar">
-            <div className="mdc-toolbar__row">
-                <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
-                    <span className="mdc-toolbar__title">Библиотека</span>
-                </section>
-                
-                <section className="mdc-toolbar__section mdc-toolbar__section--align-end">
-                    <nav className="mdc-tab-bar">
-                        <Link to="/" className="mdc-tab">Главная</Link>
-                        <Link to="/about" className="mdc-tab">О проекте</Link>
-                        <Link to="/books" className="mdc-tab">Книги</Link>
-                        {user ?
-                            <Link to="/logout" className="mdc-tab">Выйти</Link>
-                            :
-                            <Link to="/login" className="mdc-tab">Войти</Link>
-                        }
-                    </nav>
-                </section>
-            </div>
-        </header>
-    );
+	return (
+		<header className="mdc-toolbar">
+			<div className="mdc-toolbar__row">
+				<section className="mdc-toolbar__section mdc-toolbar__section--align-start">
+					<span className="mdc-toolbar__title">Библиотека</span>
+				</section>
+
+				<section className="mdc-toolbar__section mdc-toolbar__section--align-end">
+					<nav className="mdc-tab-bar">
+						<NavLink exact to="/" className="mdc-tab" activeClassName="mdc-tab--active">
+							Главная
+						</NavLink>
+						<NavLink to="/about" className="mdc-tab" activeClassName="mdc-tab--active">
+							О проекте
+						</NavLink>
+						<NavLink to="/books" className="mdc-tab" activeClassName="mdc-tab--active">
+							Книги
+						</NavLink>
+						{user ? (
+							<NavLink to="/logout" className="mdc-tab" activeClassName="mdc-tab--active">
+								Выйти
+							</NavLink>
+						) : (
+							<NavLink to="/login" className="mdc-tab" activeClassName="mdc-tab--active">
+								Войти
+							</NavLink>
+						)}
+					</nav>
+				</section>
+			</div>
+		</header>
+	);
 }
 
 export default Toolbar;
