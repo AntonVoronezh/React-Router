d9f92d4c954e3af1d9e9b35730a29ecc2fd99624
AntonVoronezh <pstut@mail.ru>
2019-05-10 12:12:51 +0300

    add BrowserRouter, Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Toolbar from './components/Toolbar';
 import Content from './components/Content';
 import Home from './pages/Home';
+import About from './pages/About';
+import Books from './pages/Books';
+import Login from './pages/Login';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="app">
-                <Toolbar />
-                
-                <Content>
-                    <Home />
-                </Content>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="app">
+					<Toolbar />
+
+					<Content>
+						<Route exact path="/" component={Home} />
+						<Route path="/about" component={About} />
+						<Route path="/books" component={Books} />
+						<Route path="/login" component={Login} />
+					</Content>
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
