3e4172c1bd32a04ae24d31980424db4f0b911cda
AntonVoronezh <pstut@mail.ru>
2019-05-10 20:41:23 +0300

    add Route in PrivitRoute

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-export default ({user, component:component, data, ...rest}) => {
-    return (
-        <Route />
-    )
-}
+export default ({ user, component: component, data, ...rest }) => {
+	return (
+		<Route {...rest} render={props => (user ? <Component data={data} {...props} /> : <Redirect to="/login" />)} />
+	);
+};
